4bc62367aa6df44f1b88fca830eb6c097992065b
GilbertoDeJesus <gadjs2001@gmail.com>
2022-10-06 17:36:53 -0500

    Progressbar update

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,4 +1,6 @@
 <script>
+import ProgressBar from './ProgressBar.vue';
+import TotalProyectos from './TotalProyectos.vue';
 export default {
     data: () => ({
         proyecto: "",
@@ -10,7 +12,6 @@ export default {
         cambiarUrgencia(proyecto, campo) {
             console.log(proyecto, campo);
             proyecto[campo] = !proyecto[campo];
-
         },
         regsitrarProyecto() {
             const proyecto = {
@@ -21,7 +22,6 @@ export default {
             };
             this.proyectos.push(proyecto);
             console.warn("Proyecto regsitrado\n", this.proyectos);
-
             this.proyecto = "";
             this.tipo = "";
             this.urgente = false;
@@ -31,30 +31,23 @@ export default {
         numeroProyectos() {
             return this.proyectos.length;
         },
-        porcentaje(){
+        porcentaje() {
             var completados = 0;
             this.proyectos.map(proyecto => {
-                if(proyecto.completado) completados++;
+                if (proyecto.completado)
+                    completados++;
             });
-
-            var value = (completados * 100) / this.numeroProyectos;
-
-            return isNaN(value) ? 0 : parseInt(value);
-            
+            return isNaN((completados * 100) / this.numeroProyectos) ? 0 : parseInt((completados * 100) / this.numeroProyectos);
         }
-    }
+    },
+    components: { ProgressBar, TotalProyectos }
 };
 </script>
 
 <template>
     <div class="row">
         <div class="col-12">
-            <h3 class="text-center">
-                Progreso {{porcentaje}}%
-            </h3>
-            <div class="progress my-4">
-                <div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" :style="{width: porcentaje+'%'}" v-bind:aria-valuenow="porcentaje" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
+            <progress-bar :porcentaje="porcentaje"/>
         </div>
         <div class="col-12 col-md-4">
             <form @submit.prevent="regsitrarProyecto" class="mt-4">
@@ -81,32 +74,7 @@ export default {
         </div>
         
         <div class="col-12 col-md-8">
-            <h3 class="my-4"> Total de proyectos: {{numeroProyectos}}</h3>
-            <div class="table-responsive">
-                <table class="table table-dark ">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Proyecto</th>
-                            <th>Tipo</th>
-                            <th>Urgente</th>
-                            <th>Completado</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(proyect, index) in proyectos">
-                            <td>{{index+1}}</td>
-                            <td>{{proyect.proyecto}}</td>
-                            <td>{{proyect.tipo}}</td>
-                            <td @click="cambiarUrgencia(proyect, 'urgente')"
-                                :class="proyect.urgente ? 'bg-success':'bg-danger'">{{proyect.urgente ? "Si":"No"}}</td>
-                            <td @click="cambiarUrgencia(proyect, 'completado')"
-                                :class="proyect.completado ?  'bg-success':'bg-danger' "> {{proyect.completado ?
-                                "Completado" : "Incompleto" }}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+            <total-proyectos :numeroProyectos="numeroProyectos" :proyectos="proyectos" :cambiarUrgencia="cambiarUrgencia" />
         </div>
     </div>
 </template>
---
+++ src/components/TotalProyectos.vue
@@ -0,0 +1,34 @@
+<script>
+    export default{
+        props:['numeroProyectos', 'proyectos', 'cambiarUrgencia'],
+    }
+</script>
+
+<template>
+    <h3 class="my-4"> Total de proyectos: {{numeroProyectos}}</h3>
+    <div class="table-responsive">
+        <table class="table table-dark ">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Proyecto</th>
+                    <th>Tipo</th>
+                    <th>Urgente</th>
+                    <th>Completado</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(proyect, index) in proyectos">
+                    <td>{{index+1}}</td>
+                    <td>{{proyect.proyecto}}</td>
+                    <td>{{proyect.tipo}}</td>
+                    <td @click="cambiarUrgencia(proyect, 'urgente')"
+                        :class="proyect.urgente ? 'bg-success':'bg-danger'">{{proyect.urgente ? "Si":"No"}}</td>
+                    <td @click="cambiarUrgencia(proyect, 'completado')"
+                        :class="proyect.completado ?  'bg-success':'bg-danger' "> {{proyect.completado ?
+                        "Completado" : "Incompleto" }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
